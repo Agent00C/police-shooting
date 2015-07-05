f91884dad23f10e21a67d86eb3919451808e200e
Joseph Topasna <josephtopasna@Joseph-Topasnas-MacBook-Pro.local>
2015-07-04 21:12:43 -0700

    modified index, map.js, main.css

--- js/map.js
+++ js/map.js
@@ -1,23 +1,34 @@
-// Function to draw your map
-var drawMap() = function() {
 
-  // Create map and set viewd
- 
+var drawMap = function() {
+	
+	var map = L.map('map').setView([31.7903, -106.4233], 10);
 
-  // Create an tile layer variable using the appropriate url
+ 	layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+	
+	layer.addTo(map);
 
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
 }
 
 // Function for getting data
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
+  var data;
+  $.ajax({
+    url:'data/response.json',
+    type: "get",
+    success:function(dat) {
+       data = dat
 
+       data.map(function(d){
+         var circle = new L.circle([d.lat, d.lng], 200, {
+         	color:'red', 
+         	opacity:0.5}).addTo(map)
+      });
+
+    }, 
+    dataType:"json"
+  }); 
 
   // When your request is successful, call your customBuild function
 
@@ -28,5 +39,3 @@ var customBuild = function() {
 
   
 }
-
-
