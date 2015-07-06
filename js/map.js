9d9946f4c4cb0cd1d1c3243350f4d9d70ec84c93
Joseph Topasna <josephtopasna@Joseph-Topasnas-MacBook-Pro.local>
2015-07-05 21:49:14 -0700

    modified index, main.css, and map.js

--- js/map.js
+++ js/map.js
@@ -1,11 +1,21 @@
+var map;
 
 var drawMap = function() {
-	
-	var map = L.map('map').setView([31.7903, -106.4233], 10);
 
+	map = L.map('map').setView([41.1456, -104.8019], 4);
+
+	var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    	maxZoom: 18,
+    	id: 'jtopasna.4a70938e',
+    	accessToken: 'sk.eyJ1IjoianRvcGFzbmEiLCJhIjoiY2M3NjNkZDFkNjdmMmIxMDI1NWI5Y2VmZWMwZGM5MDMifQ.YQy8szTvotF29FCy6ZasBQ'
+	}).addTo(map);
+
+	/*var marker = L.marker([41.1456, -104.8019]).addTo(map); /* Cheyenne, WY */
+	/* SAVE!!! 
  	layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 	
-	layer.addTo(map);
+	layer.addTo(map);*/
 
 }
 
@@ -15,15 +25,20 @@ var getData = function() {
   // Execute an AJAX request to get the data in data/response.js
   var data;
   $.ajax({
-    url:'data/response.json',
+    url:'../police-shooting/data/response.json',
     type: "get",
     success:function(dat) {
-       data = dat
+       data = dat;
 
        data.map(function(d){
-         var circle = new L.circle([d.lat, d.lng], 200, {
-         	color:'red', 
-         	opacity:0.5}).addTo(map)
+         var circle = new L.circle([d.lat, d.lng], 500, {
+    		color: 'red',
+    		fillColor: '#f03',
+    		fillOpacity: 0.5
+		 }).addTo(map);
+
+		 circle.bindPopup(d.City + ", " + d.State);
+
       });
 
     }, 
